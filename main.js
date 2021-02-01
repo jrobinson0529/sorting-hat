c612018369e5cd391cb810f52a13c2ea9e604a2e
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-01-31 20:29:30 -0600

    added some minor tweaks and styling

--- main.js
+++ main.js
@@ -51,10 +51,11 @@ const formOpen = (e) => {
   let domString = '';
   if (e.target.id === "sortingBtn") {
      domString = `
-                        <div class="form-group w-100 d-flex flex-column">
-                            <label for="name">Student:</label>
+                        <div class="form-group w-100 d-flex flex-column flex-wrap align-items-start">
+                            <label for="name" class="my-3">Student:</label>
                             <input type="text" class="form-control text-center" id="name" placeholder="Enter name here..." required>
-                            <button type="submit" class="btn btn-primary m-3" id="submit-form">Submit</button>
+                            <button type="submit" class="btn btn-primary my-3" id="submit-form">Submit</button>
+                            <img class="img-fluid align-self-end"src="./img/106-1067907_sorting-hat-png-harry-potter-sorting-hat-png.png" alt="a hat"
                           </div> 
               
                `;            
